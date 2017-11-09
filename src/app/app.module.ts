67b248cfbbdef8da8a210c88736ecbde3735cce5
Bruno Lopes <emaildobruno.lopes@gmail.com>
2017-11-09 10:42:38 -0200

    Adicionando projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { HttpModule } from '@angular/http'
+import { MatButtonModule, MatCardModule, MatToolbarModule } from '@angular/material'
+
+import { AppComponent } from './app.component'
+import { ApiService } from './api.service'
+import { MessagesComponent } from './messages.component'
+import { RegisterComponent } from './register.component'
+
+const routes = [
+    { path: 'register', component: RegisterComponent }
+
+]
+
+@NgModule({
+  declarations: [
+    AppComponent, MessagesComponent, RegisterComponent
+  ],
+  imports: [
+      BrowserModule, HttpModule, MatButtonModule, MatCardModule, MatToolbarModule, RouterModule.forRoot(routes)
+  ],
+  providers: [ApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
